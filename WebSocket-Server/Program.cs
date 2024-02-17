1c9cf443ff2afa64e1b5daa587a003ee9acb2dfb
phitruongsgu <truong30112000@gmail.com>
2024-02-17 12:38:48 +0700

    Push code full

---
+++ WebSocket-Server/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+Console.OutputEncoding = Encoding.UTF8; // viết tiếng Việt
+builder.WebHost.UseUrls("http://localhost:6969");
+var app = builder.Build();
+
+app.UseWebSockets();
+var connections = new List<WebSocket>();
+
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        var curName = context.Request.Query["name"];
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+        connections.Add(ws); // theo dõi nhiều kết nối WebSocket.
+
+        await Broadcast($"{curName} đã tham gia phòng chat");
+        await Broadcast($"{connections.Count} users đã kết nối");
+        await ReceiMessage(ws, async (result, buffer) =>
+        {
+            if (result.MessageType == WebSocketMessageType.Text)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                await Broadcast(curName + ": " + message);
+            }
+            else if (result.MessageType == WebSocketMessageType.Close || ws.State == WebSocketState.Aborted)
+            {
+                connections.Remove(ws);
+                await Broadcast($"{curName} đã rời khỏi phòng chat");
+                await Broadcast($"{connections.Count} users đã kết nối");
+                await ws.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            }
+        });
+
+
+        while (true)
+        {
+            var message = "The current time is: " + DateTime.Now.ToString("HH:mm:ss");
+            var bytes = Encoding.UTF8.GetBytes(message);
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+            if (ws.State == WebSocketState.Open)
+            {
+                await ws.SendAsync(arraySegment,
+                    WebSocketMessageType.Text,
+                    true,
+                    CancellationToken.None);
+            }
+            else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
+            {
+                break;
+            }
+            Thread.Sleep(1000);
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+    }
+});
+
+
+async Task ReceiMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+{
+    var buffer = new byte[1024 * 4];
+    while (socket.State == WebSocketState.Open)
+    {
+        var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        handleMessage(result, buffer);
+    }
+}
+
+async Task Broadcast(string message)
+{
+    var bytes = Encoding.UTF8.GetBytes(message);
+    foreach (var socket in connections)
+    {
+        if (socket.State == WebSocketState.Open)
+        {
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+            await socket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+}
+await app.RunAsync();
